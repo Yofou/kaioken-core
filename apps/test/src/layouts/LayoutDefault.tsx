fb8bf5b45f1ae314463b9053f0802ae751abda11
Nathan <reach@yofou.dev>
2024-03-25 12:47:09 -0700

    chore: Removed navbar

--- apps/test/src/layouts/LayoutDefault.tsx
+++ apps/test/src/layouts/LayoutDefault.tsx
@@ -1,9 +1,7 @@
-import { Navbar } from "$/components/Navbar"
 
 export function LayoutDefault({ children }: { children?: JSX.Element[] }) {
   return (
     <div className="flex flex-col items-center m-auto w-full min-h-screen">
-      <Navbar />
       <Content>{children}</Content>
     </div>
   )
