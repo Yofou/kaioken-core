0feb94703bd588fa4aaf48867a1a190b960b4d8b
Nathan <reach@yofou.dev>
2024-03-31 15:43:20 -0700

    version bump

--- packages/hooks/vite.config.ts
+++ packages/hooks/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     },
 
     rollupOptions: {
-      external: ['kaioken', 'kaioken/utils'],
+      external: ['kaioken', 'kaioken/dist/utils'],
       output: {
         globals: {
           "kaioken": 'Kaioken',
