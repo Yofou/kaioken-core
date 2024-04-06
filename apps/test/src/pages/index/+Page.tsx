784c168ca50fe6fcf01d900a2008546d7b0e54f3
Nathan <reach@yofou.dev>
2024-04-06 14:44:27 -0700

    chore: add window position

--- apps/test/src/pages/index/+Page.tsx
+++ apps/test/src/pages/index/+Page.tsx
@@ -1,19 +1,13 @@
 import { PageTitle } from "$/components/PageTitle"
 import {
-  useSpring
 } from "@kaioken-core/hooks"
 import { useState } from "kaioken"
 
 const Page = () => {
   const [showPageTitle, setShowPageTitle] = useState(false)
-  const [value, setValue] = useSpring(100, {
-    damping: 0.1,
-    stiffness: 0.3,
-  })
 
   const onClick = () => {
     setShowPageTitle(true)
-    setValue(x => x + 100, { hard: true }).then(() => console.log('finish'))
   }
 
   return (
---
+++ packages/hooks/src/useWindowPosition.ts
@@ -0,0 +1,22 @@
+import { useState } from "kaioken";
+import { useRafFn } from "./useRafFn";
+
+export const useWindowPosition = () => {
+  const [screenX, setScreenX] = useState(0)
+  const [screenY, setScreenY] = useState(0)
+
+  useRafFn(() => {
+    if (window.screenX !== screenX) {
+      setScreenX(window.screenX)
+    }
+
+    if (window.screenY !== screenY) {
+      setScreenY(window.screenY)
+    }
+  }, { immediate: true })
+
+  return {
+    screenX,
+    screenY,
+  }
+};
