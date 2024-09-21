f5a1b4f426dd8893ea54954ee54d20621924a0de
saranatour1 <saranatourcontact@gmail.com>
2024-09-21 22:58:19 +0300

    updated the current elemnt to use peek() in the example

--- apps/hooks-docs/src/examples/useCurrentElement.tsx
+++ apps/hooks-docs/src/examples/useCurrentElement.tsx
@@ -3,7 +3,7 @@ import { useCurrentElement } from "@kaioken-core/hooks"
 
 export const UseCurrentElementExample: Kaioken.FC = () => {
   const currentElm = useCurrentElement()
-  console.log("current element", currentElm?.value)
+  console.log("current element", currentElm!.peek())
 
   return (
     <DemoContainer className="p-4 font-cabin flex gap-4 flex-col">
