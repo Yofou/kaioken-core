faaa7436a688c2fa430afe95f1855cb31bc5c669
Nathan <39814328+Yofou@users.noreply.github.com>
2024-09-28 23:58:52 +0100

    chore: we now use private props on tween and spring, also fixed useEffectDeep docs (#31)

--- packages/hooks/src/useSpring.ts
+++ packages/hooks/src/useSpring.ts
@@ -9,28 +9,28 @@ import { tickSpring } from "./motion/spring"
 */
 
 export class SpringSignal<T> extends Signal<T> {
-  lastTime: number | undefined
-  task: Task | undefined
-  currentToken: object | undefined
-  lastValue: T
-  targetValue: T
-  invMass: number
-  invMassRecoveryRate: number
-  cancelTask: boolean
-  options: Partial<SpringOpts>
+  #lastTime: number | undefined
+  #task: Task | undefined
+  #currentToken: object | undefined
+  #lastValue: T
+  #targetValue: T
+  #invMass: number
+  #invMassRecoveryRate: number
+  #cancelTask: boolean
+  #options: Partial<SpringOpts>
 
   constructor(initial: T, options?: Partial<SpringOpts>, displayName?: string) {
     super(initial, displayName)
 
-    this.options = options ?? {}
-    this.lastTime = undefined
-    this.task = undefined
-    this.currentToken = undefined
-    this.lastValue = structuredClone(initial)
-    this.targetValue = structuredClone(initial)
-    this.invMass = 1
-    this.invMassRecoveryRate = 0
-    this.cancelTask = false
+    this.#options = options ?? {}
+    this.#lastTime = undefined
+    this.#task = undefined
+    this.#currentToken = undefined
+    this.#lastValue = structuredClone(initial)
+    this.#targetValue = structuredClone(initial)
+    this.#invMass = 1
+    this.#invMassRecoveryRate = 0
+    this.#cancelTask = false
   }
 
   #setInternal(next: T) {
@@ -40,72 +40,81 @@ export class SpringSignal<T> extends Signal<T> {
 
   set(next: T, opts = {} as Partial<SpringOpts>) {
     const spring: SpringOpts = {
-      stiffness: this.options.stiffness ?? 0.15,
-      damping: this.options.damping ?? 0.8,
-      precision: this.options.precision ?? 0.01,
+      stiffness: this.#options.stiffness ?? 0.15,
+      damping: this.#options.damping ?? 0.8,
+      precision: this.#options.precision ?? 0.01,
       ...opts,
     }
 
     const newValue = next
 
-    this.targetValue = newValue
-    this.currentToken = {}
-    const token = this.currentToken
+    this.#targetValue = newValue
+    this.#currentToken = {}
+    const token = this.#currentToken
     if (
       newValue == null ||
       opts.hard ||
       (spring.stiffness >= 1 && spring.damping >= 1)
     ) {
-      this.cancelTask = true
-      this.lastTime = raf.now()
-      this.lastValue = newValue
-      this.#setInternal(this.targetValue)
+      this.#cancelTask = true
+      this.#lastTime = raf.now()
+      this.#lastValue = newValue
+      this.#setInternal(this.#targetValue)
       return Promise.resolve()
     } else if (opts.soft) {
       const rate = opts.soft === true ? 0.5 : +opts.soft
-      this.invMassRecoveryRate = 1 / (rate * 60)
-      this.invMass = 0 // infinite mass, unaffected by spring forces
+      this.#invMassRecoveryRate = 1 / (rate * 60)
+      this.#invMass = 0 // infinite mass, unaffected by spring forces
     }
 
-    if (!this.task) {
-      this.lastTime = raf.now()
-      this.cancelTask = false
+    if (!this.#task) {
+      this.#lastTime = raf.now()
+      this.#cancelTask = false
 
-      this.task = loop((now) => {
-        if (this.cancelTask) {
-          this.cancelTask = false
-          this.task = undefined
+      this.#task = loop((now) => {
+        if (this.#cancelTask) {
+          this.#cancelTask = false
+          this.#task = undefined
           return false
         }
-        this.invMass = Math.min(this.invMass + this.invMassRecoveryRate, 1)
+        this.#invMass = Math.min(this.#invMass + this.#invMassRecoveryRate, 1)
         const ctx: TickContext = {
-          inv_mass: this.invMass,
+          inv_mass: this.#invMass,
           opts: spring,
           settled: true,
-          dt: ((now - (this.lastTime ?? raf.now())) * 60) / 1000,
+          dt: ((now - (this.#lastTime ?? raf.now())) * 60) / 1000,
         }
         const nextValue = tickSpring(
           ctx,
-          this.lastValue,
+          this.#lastValue,
           this.value,
-          this.targetValue
+          this.#targetValue
         )
-        this.lastTime = now
-        this.lastValue = this.value
+        this.#lastTime = now
+        this.#lastValue = this.value
 
         this.#setInternal(nextValue)
         if (ctx.settled) {
-          this.task = undefined
+          this.#task = undefined
         }
         return !ctx.settled
       })
     }
 
     return new Promise((fulfil) => {
-      this.task?.promise.then(() => {
-        if (token === this.currentToken) fulfil(undefined)
+      this.#task?.promise.then(() => {
+        if (token === this.#currentToken) fulfil(undefined)
       })
     })
+  }
+
+  /**
+   * @description cancels the request animation frame loop that's handling interpolation, It's likely you'll never need to call this yourself
+   */
+  abortTask() {
+    if (this.#task) {
+      this.#task.abort()
+    }
   }
 }
 
@@ -130,10 +139,7 @@ export const spring = <T>(
         if (isInit) {
           hook.signal = internalSignal
           hook.cleanup = () => {
-            if (hook.signal.task) {
-              hook.signal.task.abort()
-            }
-
+            hook.signal.abortTask()
             SpringSignal.subscribers(hook.signal).clear()
           }
         }
