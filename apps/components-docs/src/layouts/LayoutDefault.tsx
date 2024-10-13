1fa195b49752433e9c0a9bfb38aa543568f0ae21
Yofou <reach@yofou.dev>
2024-10-13 09:56:11 +0100

    chore: update context menyu

--- apps/components-docs/src/layouts/LayoutDefault.tsx
+++ apps/components-docs/src/layouts/LayoutDefault.tsx
@@ -1,9 +1,6 @@
-import { Navbar } from "$/components/Navbar"
-
 export function LayoutDefault({ children }: { children: JSX.Children }) {
   return (
-    <div className="flex flex-col items-center m-auto w-full min-h-screen">
-      <Navbar />
+    <div className="flex flex-col items-center m-auto w-full min-h-screen py-[2rem] px-4">
       <div className="p-5 pb-10">{children}</div>
     </div>
   )
--- apps/components-docs/src/pages/index/+Page.tsx
+++ apps/components-docs/src/pages/index/+Page.tsx
@@ -1,4 +1,5 @@
-import { ContextMenu } from "@kaioken-core/components"
+import { ContextMenu, Tooltip } from "@kaioken-core/components"
+import { signal, useEffect } from "kaioken"
 
 export { Page }
 
@@ -17,9 +18,7 @@ const TestContextMenu = () => {
         <ContextMenu.Content
           className={"w-[300px] h-[500px] p-10 flex flex-col gap-2"}
         >
-          <ContextMenu.Item asChild>
-            <button>button 1</button>
-          </ContextMenu.Item>
+          <ContextMenu.Item>button 1</ContextMenu.Item>
           <ContextMenu.Item asChild>
             <button>button 2</button>
           </ContextMenu.Item>
@@ -32,10 +31,35 @@ const TestContextMenu = () => {
   )
 }
 
+// TOOLTIP IS CURRENTLY ALWAYS AS CHILD, WE NEED TOO CHANGE
+const TestTooltipMenu: Kaioken.FC = () => {
+  const isOpen = signal(false)
+
+  return (
+    <Tooltip.Root
+      open={isOpen}
+      sideGap={16}
+      side="right"
+      avoidCollisions={true}
+    >
+      <Tooltip.Trigger>
+        <button className={"w-[50px] ml-[50px]"}>Boop</button>
+      </Tooltip.Trigger>
+
+      <Tooltip.Content>
+        <div className={"backdrop:hidden w-[100px] h-[100px] m-0 p-0"}>
+          boop
+        </div>
+      </Tooltip.Content>
+    </Tooltip.Root>
+  )
+}
+
 function Page() {
   return (
     <div className={"flex flex-col gap-4"}>
       <div className={"w-full"} />
+      {/*  <TestTooltipMenu />  */}
       <TestContextMenu />
     </div>
   )
