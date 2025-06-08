86557f05b43297fb8bb84d34e54ac39ecb743ec1
LankyMoose <rausten93@gmail.com>
2025-06-08 19:07:39 +1200

    chore: UseIntersectionObserver example now shows correct scroll direction

--- apps/hooks-docs/src/examples/useIntersectionObserver.tsx
+++ apps/hooks-docs/src/examples/useIntersectionObserver.tsx
@@ -4,14 +4,20 @@ import { useSignal, useRef } from "kaioken"
 
 export const UseIntersectionObserverExample: Kaioken.FC = () => {
   const isVisible = useSignal(false)
-
+  const dir = useSignal("down")
+  const containerRef = useRef<HTMLDivElement>(null)
   const ref = useRef<HTMLDivElement>(null)
-  const controls = useIntersectionObserver(ref, (intersections) => {
+
+  useIntersectionObserver(ref, (intersections) => {
     isVisible.value = intersections[0].isIntersecting
+    const containerHeight = containerRef.current!.scrollHeight
+    const top = intersections[0].boundingClientRect.top
+    dir.value = top < containerHeight / 2 ? "up" : "down"
   })
+
   return (
     <DemoContainer>
-      <div className="w-full h-[300px] overflow-y-scroll">
+      <div ref={containerRef} className="w-full h-[300px] overflow-y-scroll">
         <div className="mb-[600px]" />
         <div ref={ref}>
           <p className="text-center">When viewing me...</p>
@@ -22,7 +28,10 @@ export const UseIntersectionObserverExample: Kaioken.FC = () => {
       <p className="text-center mt-4 text-[1.5rem] font-bold">
         Element is {!isVisible.value && "not"} in view
       </p>
-      <p className="text-center mb-4 font-semibold">Scroll down</p>
+      <p
+        className="text-center mb-4 font-semibold"
+        innerHTML={isVisible.value ? "&nbsp;" : `Scroll ${dir}`}
+      />
     </DemoContainer>
   )
 }
